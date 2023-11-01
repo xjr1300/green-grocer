cabeb02e3929d8c695f5552bc31931637967dc78
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-11-01 09:32:20 +0900

    Implement a Vegetable struct.

--- domain/src/models/vegetable.rs
+++ domain/src/models/vegetable.rs
@@ -1,5 +1,6 @@
 use uuid::Uuid;
 
+use super::primitives::Price;
 use macros::EntityId;
 
 /// 野菜ID
@@ -15,7 +16,7 @@ pub struct Vegetable {
     /// 野菜名
     name: String,
     /// 単価
-    unit_price: u32,
+    unit_price: Price,
 }
 
 impl Vegetable {
@@ -29,7 +30,7 @@ impl Vegetable {
     /// # 戻り値
     ///
     /// 野菜
-    pub fn new(name: &str, unit_price: u32) -> Self {
+    pub fn new(name: &str, unit_price: Price) -> Self {
         Self {
             id: VegetableId::default(),
             name: name.to_string(),
@@ -37,30 +38,30 @@ impl Vegetable {
         }
     }
 
-    /// 野菜IDを返却する。
+    /// 野菜IDを返す。
     ///
     /// # 戻り値
     ///
-    /// 野菜ID。
+    /// 野菜ID
     pub fn id(&self) -> VegetableId {
         self.id
     }
 
-    /// 野菜名を返却する。
+    /// 野菜名を返す。
     ///
     /// # 戻り値
     ///
-    /// 野菜名。
+    /// 野菜名
     pub fn name(&self) -> &str {
         &self.name
     }
 
-    /// 単価を返却する。
+    /// 単価を返す。
     ///
     /// # 戻り値
     ///
-    /// 単価。
-    pub fn unit_price(&self) -> u32 {
+    /// 単価
+    pub fn unit_price(&self) -> Price {
         self.unit_price
     }
 }
