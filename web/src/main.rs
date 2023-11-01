d606adea4b8658bbc9094f32e36c93957b4477cb
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-11-01 10:20:33 +0900

    Implement a feature to connect database.

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,12 +1,24 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 use controller::health_check::health_check;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
-        .bind(("127.0.0.1", 8001))?
-        .run()
-        .await
-        .map_err(|e| anyhow::anyhow!(e))
+    dotenvy::dotenv()?;
+
+    // データベース接続プールを作成
+    let database_url = std::env::var("DATABASE_URL")?;
+    let pool = PgPoolOptions::new().connect(&database_url).await?;
+
+    // Webアプリケーションサーバを起動
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(health_check)
+    })
+    .bind(("127.0.0.1", 8001))?
+    .run()
+    .await
+    .map_err(|e| anyhow::anyhow!(e))
 }
