9a4789b37cd2dff37dae10d9af85c9f06ed0936d
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-11-01 09:45:00 +0900

    Implement Sale and SaleDetail structs.

--- domain/src/models/mod.rs
+++ domain/src/models/mod.rs
@@ -1,2 +1,3 @@
 pub mod primitives;
 pub mod sales;
+pub mod vegetable;
--- domain/src/models/sales.rs
+++ domain/src/models/sales.rs
@@ -3,6 +3,8 @@ use std::hash::Hash;
 use time::OffsetDateTime;
 use uuid::Uuid;
 
+use super::primitives::{Price, Quantity};
+use super::vegetable::Vegetable;
 use macros::EntityId;
 
 /// 販売ID
@@ -17,12 +19,44 @@ pub struct Sale {
     id: SaleId,
     /// 販売日時
     sold_at: OffsetDateTime,
+    /// 販売明細
+    sale_details: Vec<SaleDetail>,
     /// 合計販売金額
-    total_amount: u32,
+    total_price: u32,
+    /// 作成日時
+    created_at: OffsetDateTime,
+    /// 更新日時
+    updated_at: Option<OffsetDateTime>,
 }
 
 impl Sale {
-    /// 販売IDを返却する。
+    /// 販売を構築する。
+    ///
+    /// # 引数
+    ///
+    /// * `sold_at` - 販売日時
+    /// * `sale_details` - 販売明細
+    ///
+    /// # 戻り値
+    ///
+    /// 販売
+    pub fn new(sold_at: OffsetDateTime, sale_details: Vec<SaleDetail>) -> Self {
+        let total_price = sale_details
+            .iter()
+            .map(|sd| sd.sold_unit_price().value() * sd.sold_quantity().value())
+            .sum();
+
+        Self {
+            id: SaleId::default(),
+            sold_at,
+            sale_details,
+            total_price,
+            created_at: OffsetDateTime::now_utc(),
+            updated_at: None,
+        }
+    }
+
+    /// 販売IDを返す。
     ///
     /// # 戻り値
     ///
@@ -31,7 +65,7 @@ impl Sale {
         self.id
     }
 
-    /// 販売日時を返却する。
+    /// 販売日時を返す。
     ///
     /// # 戻り値
     ///
@@ -40,12 +74,113 @@ impl Sale {
         self.sold_at
     }
 
-    /// 合計販売金額を返却する。
+    /// 販売明細を返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 販売明細を格納したベクタ
+    pub fn sale_details(&self) -> &[SaleDetail] {
+        &self.sale_details
+    }
+
+    /// 合計販売金額を返す。
     ///
     /// # 戻り値
     ///
     /// 合計販売金額。
-    pub fn total_amount(&self) -> u32 {
-        self.total_amount
+    pub fn total_price(&self) -> u32 {
+        self.total_price
+    }
+
+    /// 作成日時を返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 作成日時
+    pub fn created_at(&self) -> OffsetDateTime {
+        self.created_at
+    }
+
+    /// 更新日時を返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 更新日時
+    pub fn updated_at(&self) -> Option<OffsetDateTime> {
+        self.updated_at
+    }
+}
+
+/// 販売明細ID
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, EntityId)]
+pub struct SaleDetailId {
+    value: Uuid,
+}
+
+/// 販売明細
+pub struct SaleDetail {
+    /// 販売明細ID
+    id: SaleDetailId,
+    /// 販売した野菜
+    vegetable: Vegetable,
+    /// 野菜を販売した単価
+    sold_unit_price: Price,
+    /// 野菜を販売した数量
+    sold_quantity: Quantity,
+}
+
+impl SaleDetail {
+    /// 販売明細を構築する。
+    ///
+    /// # 引数
+    ///
+    /// * `vegetable` - 販売した野菜
+    /// * `sold_unit_price` - 野菜を販売した単価
+    /// * `sold_quantity` - 野菜を販売した数量
+    ///
+    /// # 戻り値
+    pub fn new(vegetable: Vegetable, sold_unit_price: Price, sold_quantity: Quantity) -> Self {
+        Self {
+            id: SaleDetailId::default(),
+            vegetable,
+            sold_unit_price,
+            sold_quantity,
+        }
+    }
+
+    /// 販売明細IDを返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 販売明細ID
+    pub fn id(&self) -> SaleDetailId {
+        self.id
+    }
+
+    /// 販売した野菜を返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 販売した野菜
+    pub fn vegetable(&self) -> &Vegetable {
+        &self.vegetable
+    }
+
+    /// 野菜を販売した単価を返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 野菜を販売した時の単価
+    pub fn sold_unit_price(&self) -> Price {
+        self.sold_unit_price
+    }
+
+    /// 野菜を販売した数量を返す。
+    ///
+    /// # 戻り値
+    ///
+    /// 野菜を販売した数量
+    pub fn sold_quantity(&self) -> Quantity {
+        self.sold_quantity
     }
 }
