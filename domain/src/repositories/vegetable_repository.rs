e917fb30a9a71838f761818a3d3939d7271c93f1
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-11-01 14:08:33 +0900

    Fix the VegetableRepository trait to annotate async_trait.

--- domain/src/repositories/vegetable_repository.rs
+++ domain/src/repositories/vegetable_repository.rs
@@ -1,26 +1,44 @@
+use async_trait::async_trait;
+
 use crate::models::primitives::Price;
 use crate::models::vegetable::{Vegetable, VegetableId};
 
 /// 登録または更新する野菜
-pub struct EditVegetable {
+pub struct UpsertVegetable {
     /// 野菜ID
-    id: VegetableId,
+    pub id: VegetableId,
     /// 野菜名
-    name: String,
+    pub name: String,
     /// 単価
-    unit_price: Price,
+    pub unit_price: Price,
+}
+
+/// 部分更新する野菜
+pub struct PartialVegetable {
+    /// 野菜ID
+    pub id: VegetableId,
+    /// 野菜名
+    pub name: Option<String>,
+    /// 単価
+    pub unit_price: Option<Price>,
 }
 
 /// 野菜リポジトリ
+#[async_trait]
 pub trait VegetableRepository {
     /// 野菜IDで指定した野菜を検索する。
-    fn find_by_id(&self, id: VegetableId) -> anyhow::Result<Vegetable>;
+    async fn find_by_id(&self, id: VegetableId) -> anyhow::Result<Option<Vegetable>>;
     /// すべての野菜を検索する。
-    fn find_all(&self) -> anyhow::Result<Vec<Vegetable>>;
+    async fn find_all(&self) -> anyhow::Result<Vec<Vegetable>>;
     /// 野菜を登録する。
-    fn register(&self, vegetable: EditVegetable) -> anyhow::Result<Vegetable>;
+    async fn register(&self, vegetable: UpsertVegetable) -> anyhow::Result<Vegetable>;
     /// 野菜を更新する。
-    fn update(&self, vegetable: EditVegetable) -> anyhow::Result<Vegetable>;
+    async fn update(&self, vegetable: UpsertVegetable) -> anyhow::Result<Option<Vegetable>>;
+    /// 野菜を部分更新する。
+    async fn partial_update(
+        &self,
+        vegetable: PartialVegetable,
+    ) -> anyhow::Result<Option<Vegetable>>;
     /// 野菜IDで指定した野菜を削除する。
-    fn delete(&self, id: VegetableId) -> anyhow::Result<()>;
+    async fn delete(&self, id: VegetableId) -> anyhow::Result<u32>;
 }
