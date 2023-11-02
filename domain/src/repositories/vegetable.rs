d67f491c93ab4fa4d19514ccd2ac40d3f71d1e09
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-11-02 10:06:58 +0900

    Cleanup the VegetableRepository trait.

--- domain/src/repositories/vegetable.rs
+++ domain/src/repositories/vegetable.rs
@@ -21,25 +21,30 @@ pub struct PartialVegetable {
 
 /// 野菜リポジトリ
 #[async_trait]
-pub trait VegetableRepository: Send + Sync + 'static {
+pub trait VegetableRepository: 'static {
     /// 野菜IDで指定した野菜を検索する。
     async fn find_by_id(&self, id: VegetableId) -> anyhow::Result<Option<Vegetable>>;
+
     /// すべての野菜を検索する。
     async fn find_all(&self) -> anyhow::Result<Vec<Vegetable>>;
+
     /// 野菜を登録する。
     async fn register(&self, vegetable: UpsertVegetable) -> anyhow::Result<Vegetable>;
+
     /// 野菜を更新する。
     async fn update(
         &self,
         id: VegetableId,
         vegetable: UpsertVegetable,
     ) -> anyhow::Result<Option<Vegetable>>;
+
     /// 野菜を部分更新する。
     async fn partial_update(
         &self,
         id: VegetableId,
         vegetable: PartialVegetable,
     ) -> anyhow::Result<Option<Vegetable>>;
+
     /// 野菜IDで指定した野菜を削除する。
     async fn delete(&self, id: VegetableId) -> anyhow::Result<u32>;
 }
